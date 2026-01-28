062b2a70b7cd8ffee5197f66150736444007c5da
Byl0chkaa <sakalo.vicky@gmail.com>
2026-01-28 17:23:23 +0200

    commit hw1/task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css'
 
-
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
 function App() {
 
     return (
         <>
-
+<ul>
+    {
+        coursesTitleArray.map((course, index) => <li key={index}>{course}</li>)
+    }
+</ul>
         </>
     )
 }
