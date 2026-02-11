2b30607cb94d919857a0268416d81414d467c1dc
Byl0chkaa <sakalo.vicky@gmail.com>
2026-02-11 23:25:56 +0200

    commit h2-task4.2

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -4,7 +4,7 @@ import type {PostModel} from "../models/PostModel.ts";
 const postsUrl = import.meta.env.VITE_POSTS_URL + '/posts';
 
 export const loadPosts = async ():Promise<PostModel[]> => {
-    const response<ResponsePostDummyjson> = await fetch(postsUrl)
+    const response:ResponsePostDummyjson = await fetch(postsUrl)
         .then(result => result.json());
     return response.posts;
 }
