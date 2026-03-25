ae14c13830523bcf77dc4dcdf0a93aaa577ec0f0
Byl0chkaa <sakalo.vicky@gmail.com>
2026-03-25 13:21:52 +0200

    hw3/task1.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./routes/router.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {UserPage} from "./pages/UserPage.tsx";
+import {PostsPage} from "./pages/PostsPage.tsx";
+import {CommentsPage} from "./pages/CommentsPage.tsx";
+import {ProductsPage} from "./pages/ProductsPage.tsx";
+import {MainPage} from "./pages/MainPage.tsx";
 
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
+createRoot(document.getElementById('root')!).render(<BrowserRouter>
+    <Routes>
+        <Route path={'/'} element={<MainPage/>}>
+            <Route path={'users'} element={<UserPage/>}></Route>
+            <Route path={'posts'} element={<PostsPage/>}></Route>
+            <Route path={'comments'} element={<CommentsPage/>}></Route>
+            <Route path={'products'} element={<ProductsPage/>}></Route>
+        </Route>
+    </Routes>
+</BrowserRouter>)
